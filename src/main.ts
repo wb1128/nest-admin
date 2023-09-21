88c8afebe378dfb0d5b59d649955ed2f4f3f1018
weibin <372618291@qq.com>
2023-09-21 18:25:59 +0800

    feat: 添加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('api文档')
+    .setDescription('给前端提供支持')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger-api', app, document);
   await app.listen(3000);
 }
 bootstrap();
